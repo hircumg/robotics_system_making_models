4428f8f5777dbc2a72996d46603ca9fa2aab2bd9
hircumg <hircumg@gmail.com>
2019-02-05 15:03:30 +0300

    made function for founding max size of the object

--- reading.py
+++ reading.py
@@ -14,9 +14,11 @@ def slice(mesh, bottom, upper):
     pass
 
 def find_mins_maxs(mesh):
-    points = np.reshape(mesh.vectors, (-1,3))
-    print(points)
-    print(points.shape)
+    decimals = 6
+    points = np.reshape(mesh.vectors, (-1, 3))
+    points = np.around(points - 10 ** (-(decimals + 5)), decimals=decimals)
+    # print(points)
+    # print(points.shape)
     x_min = None
     x_max = None
     y_min = None
@@ -39,24 +41,25 @@ def find_mins_maxs(mesh):
                 x_min = point[0]
 
             # find y borders
-            if (x_max < point[0]):
-                x_max = point[0]
-            elif(x_min > point[0]):
-                x_min = point[0]
+            if (y_max < point[1]):
+                y_max = point[1]
+            elif(y_min > point[1]):
+                y_min = point[1]
 
             #  find z borders
-            if (x_max < point[0]):
-                x_max = point[0]
-            elif(x_min > point[0]):
-                x_min = point[0]
+            if (z_max < point[2]):
+                z_max = point[2]
+            elif(z_min > point[2]):
+                z_min = point[2]
 
+    return x_min, x_max, y_min, y_max, z_min, z_max
 
 # Load the STL files
 your_mesh = mesh.Mesh.from_file('Models/PLA_190to220_stl_file.stl')
 
 mesh_points = your_mesh.points
 # print(mesh_points)
-find_mins_maxs(your_mesh)
+print(find_mins_maxs(your_mesh))
 # print(your_mesh.vectors)
 # print(your_mesh.normals)
 # print(find_mins_maxs(your_mesh))
@@ -65,17 +68,15 @@ find_mins_maxs(your_mesh)
 
 
 
-# # Create a new plot
-# figure = pyplot.figure()
-# axes = mplot3d.Axes3D(figure)
-#
+# Create a new plot
+figure = pyplot.figure()
+axes = mplot3d.Axes3D(figure)
 
-# axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-#
-# # Auto scale to the mesh size
-# scale = your_mesh.points.flatten(-1)
-# # print(scale)
-# axes.auto_scale_xyz(scale, scale, scale)
-# # Show the plot to the screen
-# pyplot.show()
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+
+# Auto scale to the mesh size
+scale = your_mesh.points.flatten('C')
+axes.auto_scale_xyz(scale, scale, scale)
+# Show the plot to the screen
+pyplot.show()
 

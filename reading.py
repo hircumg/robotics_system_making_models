9ee25415595117b44a5990c8e71c9375c0bc38c8
hircumg <hircumg@gmail.com>
2019-02-05 19:07:21 +0300

    added docs for function find_mins_maxs

--- reading.py
+++ reading.py
@@ -7,18 +7,24 @@ from mpl_toolkits import mplot3d
 def one_slice(mesh, heigth):
     pass
 
+
 def one_height(mesh):
     pass
+
 
 def slice(mesh, bottom, upper):
     pass
 
-def find_mins_maxs(mesh):
-    decimals = 6
+
+def find_mins_maxs(mesh, decimals = 6):
+    '''
+    Find borders of given 3d object
+    :param mesh: mesh of given 3d object
+    :param decimals: number of decimal places to round to
+    :return: borders in all axes
+    '''
     points = np.reshape(mesh.vectors, (-1, 3))
     points = np.around(points - 10 ** (-(decimals + 5)), decimals=decimals)
-    # print(points)
-    # print(points.shape)
     x_min = None
     x_max = None
     y_min = None
@@ -26,7 +32,7 @@ def find_mins_maxs(mesh):
     z_min = None
     z_max = None
     for point in points:
-        if(x_max is None):
+        if (x_max is None):
             x_max = point[0]
             y_max = point[0]
             x_min = point[1]
@@ -37,22 +43,23 @@ def find_mins_maxs(mesh):
             # find x borders
             if (x_max < point[0]):
                 x_max = point[0]
-            elif(x_min > point[0]):
+            elif (x_min > point[0]):
                 x_min = point[0]
 
             # find y borders
             if (y_max < point[1]):
                 y_max = point[1]
-            elif(y_min > point[1]):
+            elif (y_min > point[1]):
                 y_min = point[1]
 
             #  find z borders
             if (z_max < point[2]):
                 z_max = point[2]
-            elif(z_min > point[2]):
+            elif (z_min > point[2]):
                 z_min = point[2]
 
     return x_min, x_max, y_min, y_max, z_min, z_max
+
 
 # Load the STL files
 your_mesh = mesh.Mesh.from_file('Models/PLA_190to220_stl_file.stl')
@@ -63,9 +70,6 @@ print(find_mins_maxs(your_mesh))
 # print(your_mesh.vectors)
 # print(your_mesh.normals)
 # print(find_mins_maxs(your_mesh))
-
-
-
 
 
 # Create a new plot
@@ -79,4 +83,3 @@ scale = your_mesh.points.flatten('C')
 axes.auto_scale_xyz(scale, scale, scale)
 # Show the plot to the screen
 pyplot.show()
-

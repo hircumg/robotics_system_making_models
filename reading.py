1ec9033cd7af906e0f48deb4ed06e07d22403f1d
hircumg <hircumg@gmail.com>
2019-02-07 23:39:49 +0300

    initially made converting to separeted lines

--- reading.py
+++ reading.py
@@ -1,14 +1,152 @@
 from stl import mesh
 import numpy as np
 from matplotlib import pyplot
+import matplotlib.pyplot as plt
+
 from mpl_toolkits import mplot3d
 
 
-def one_slice(mesh, heigth):
-    pass
+def plot_lines(lines):
+
+    for line in lines:
+        plt.plot([line[0][0], line[1][0]], [line[0][1], line[1][1]], marker='o', markerfacecolor='red', markersize=5, color='skyblue', linewidth=4)
+
+    plt.show()
 
 
-def one_height(mesh):
+def plot_vectors(vectors):
+    new_vectors = []
+    for vec in vectors:
+        new_vectors.append(vec[0])
+
+    new_vectors = np.array(new_vectors)
+
+    # print(new_vectors.shape)
+    # print(new_vectors)
+
+    data = np.zeros(len(new_vectors), dtype=mesh.Mesh.dtype)
+    data['vectors'] = new_vectors
+
+    cube = mesh.Mesh(data)
+    cube.save('test.stl')
+    # Create a new plot
+    figure = pyplot.figure()
+    axes = mplot3d.Axes3D(figure)
+
+    # Render the cube
+    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(cube.vectors))
+
+    # Auto scale to the mesh size
+    scale = cube.points.flatten(-1)
+    axes.auto_scale_xyz(scale, scale, scale)
+
+    # Show the plot to the screen
+    pyplot.show()
+
+def one_slice(vectors, heigth, length, width, box_size, decimals= 6, frac= 0.001):
+    vectors = np.around(vectors - 10 ** (-(decimals + 5)), decimals=decimals)
+    lines_of_slice = []
+    triangles_of_slice = []
+
+    # left only triangles which crosses horizontal plane
+    for triangle in vectors:
+        upper = 0
+        lower = 0
+        for i in triangle:
+            if i[2] > heigth:
+                upper += 1
+            else:
+                lower += 1
+        if upper != 0 and lower != 0:
+            triangles_of_slice.append([triangle.copy(), upper])
+    # triangles_of_slice = np.array(triangles_of_slice)
+    # print( np.array(triangles_of_slice).shape)
+    plot_vectors(triangles_of_slice)
+
+
+
+    # save only line of triangles in horizontal plane
+    for triangle, upper in triangles_of_slice:
+
+        # find points for neadble lines
+        point_m = None
+        point_a = None
+        point_b = None
+        if upper == 2:
+    #         two points upper than height
+            min_ind = 0
+            for i in range(2):
+                if triangle[min_ind][2] > triangle[i+1][2]:
+                    min_ind = i+1
+            point_m = triangle[min_ind]
+            point_a = triangle[(min_ind+1)%3]
+            point_b = triangle[(min_ind+2)%3]
+
+        else:
+    #         two points lower than height
+            max_ind = 0
+            for i in range(2):
+                if triangle[max_ind][2] < triangle[i+1][2]:
+                    max_ind = i+1
+            point_m = triangle[max_ind]
+            point_a = triangle[(max_ind+1)%3]
+            point_b = triangle[(max_ind+2)%3]
+        # print(point_m[2], point_a[2], point_b[2], upper)
+
+
+    #     find intersections between lines and plane
+    #     with a line
+        z = heigth
+        x_a = point_m[0] + (point_a[0] - point_m[0]) * (z - point_m[2]) / (point_a[2] - point_m[2])
+        y_a = point_m[1] + (point_a[1] - point_m[1]) * (z - point_m[2]) / (point_a[2] - point_m[2])
+
+        x_b = point_m[0] + (point_b[0] - point_m[0]) * (z - point_m[2]) / (point_b[2] - point_m[2])
+        y_b = point_m[1] + (point_b[1] - point_m[1]) * (z - point_m[2]) / (point_b[2] - point_m[2])
+        lines_of_slice.append([[x_a, y_a],[x_b, y_b]])
+    lines_of_slice = np.array(lines_of_slice)
+    lines_of_slice = np.around(lines_of_slice - 10 ** (-(decimals + 5)), decimals=decimals)
+    # print(lines_of_slice)
+    # print(lines_of_slice.shape)
+    plot_lines(lines_of_slice)
+
+
+
+
+    # separate borders
+    separated_lines_of_slice = []
+    for time in range(1000):
+        if len(lines_of_slice) == 0:
+            break
+        closed_line_of_slice = [lines_of_slice[0]]
+        last_point = lines_of_slice[0][1]
+        lines_of_slice = np.delete(lines_of_slice, 0, 0)
+        # print(last_line)
+        next_index = 1
+        while next_index != None:
+            next_index = None
+            for i in range(lines_of_slice.shape[0]):
+                temp_line = lines_of_slice[i]
+                if abs(temp_line[0][0] - last_point[0]) <= frac and abs(temp_line[0][1] - last_point[1]) <= frac :
+                    next_index = i
+                    closed_line_of_slice.append(lines_of_slice[next_index])
+                    last_point = lines_of_slice[next_index][1]
+                    lines_of_slice = np.delete(lines_of_slice, next_index, 0)
+                    break
+                elif abs(temp_line[1][0] - last_point[0]) <= frac and abs(temp_line[1][1] - last_point[1]) <= frac :
+                    next_index = i
+                    closed_line_of_slice.append(lines_of_slice[next_index])
+                    last_point = lines_of_slice[next_index][0]
+                    lines_of_slice = np.delete(lines_of_slice, next_index, 0)
+                    break
+
+
+            # print(next_index, last_line)
+        separated_lines_of_slice.append(closed_line_of_slice)
+
+    print(len(separated_lines_of_slice))
+    return None
+
+def one_height_slice(mesh):
     pass
 
 
@@ -62,24 +200,28 @@ def find_mins_maxs(mesh, decimals = 6):
 
 
 # Load the STL files
-your_mesh = mesh.Mesh.from_file('Models/PLA_190to220_stl_file.stl')
+# your_mesh = mesh.Mesh.from_file('Models/PLA_190to220_stl_file.stl')
+your_mesh = mesh.Mesh.from_file('Models/Minecraft_Hanger_hand_1.stl')
+# your_mesh = mesh.Mesh.from_file('Models/xyzCalibration_cube.stl')
 
-mesh_points = your_mesh.points
-# print(mesh_points)
-print(find_mins_maxs(your_mesh))
-# print(your_mesh.vectors)
+
+x_min, x_max, y_min, y_max, z_min, z_max = find_mins_maxs(your_mesh)
+
+one_slice(your_mesh.vectors, 3, abs(x_max-x_min), abs(y_max-y_min),10)
+one_slice(your_mesh.vectors, 13, abs(x_max-x_min), abs(y_max-y_min),10)
+# one_slice(your_mesh.vectors, 19, abs(x_max-x_min), abs(y_max-y_min),10)
 # print(your_mesh.normals)
-# print(find_mins_maxs(your_mesh))
+print(find_mins_maxs(your_mesh))
 
 
-# Create a new plot
-figure = pyplot.figure()
-axes = mplot3d.Axes3D(figure)
-
-axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-
-# Auto scale to the mesh size
-scale = your_mesh.points.flatten('C')
-axes.auto_scale_xyz(scale, scale, scale)
-# Show the plot to the screen
-pyplot.show()
+# # Create a new plot
+# figure = pyplot.figure()
+# axes = mplot3d.Axes3D(figure)
+#
+# axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+#
+# # Auto scale to the mesh size
+# scale = your_mesh.points.flatten('C')
+# axes.auto_scale_xyz(scale, scale, scale)
+# # Show the plot to the screen
+# pyplot.show()
